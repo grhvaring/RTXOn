7244e05a207ba0d3d5bb0742c0d986457944974e
grhvaring <grhvaring@gmail.com>
2022-03-20 09:51:26 +0100

    Add methods ParseImgSize, ParseEndianness, writeFloat and writePfm

--- RTXLib/HdrImage.cs
+++ RTXLib/HdrImage.cs
@@ -2,6 +2,8 @@
 // The image is implemented as a monodimensional array of the struct Color.
 // Each element of the array represents the color of a pixel.
 
+using System.Text;
+
 namespace RTXLib;
 
 public class HdrImage
@@ -53,7 +55,7 @@ public class HdrImage
 
 	public bool  valid_coordinates(int x, int y)
 	{
-		return ((x < 0) || (x > Width) || (y < 0) || (y > Height));
+		return ((x >= 0) && (x <= Width) && (y >= 0) && (y <= Height));
 	}
 
 	// pixel_offset converts the coordinates (x,y) of a pixel in a positional index
@@ -76,6 +78,94 @@ public class HdrImage
     {
 		return Pixels[pixel_offset(x, y)];
     }
+	
+	// Read the dimention of a PFM image from a string
+
+	public void ParseImgSize(string line)
+	{
+		char[] delimiterChars = { ' ', '\t' };
+		string[] dimentions = line.Split(delimiterChars);
+
+		if (dimentions.Length != 2)
+		{
+			throw new InvalidPfmFileFormat("Invalid image size specification: it should be <width> <height>.");
+		}
+
+		try
+		{
+			Width = int.Parse(dimentions[0]);
+			Height = int.Parse(dimentions[1]);
+			if (Width < 0 || Height < 0)
+			{
+				throw new InvalidDataException();
+			}
+		}
+		catch
+		{
+			throw new InvalidPfmFileFormat("Invalid width or height. Should be two int separated by a whitespace.");
+		}
+	} 
+	
+	// Decode endianness of from a string
+	public float ParseEndianness(string line)
+	{
+		float value;
+		string message = "Invalid endianness specification. Should be a single non-zero number.";
+		try
+		{
+			value = float.Parse(line);
+		}
+		catch
+		{
+			throw new InvalidPfmFileFormat(message);
+		}
+
+		if (value > 0)
+		{
+			return +1.0f;
+		}
+		else if (value < 0)
+		{
+			return -1.0f;
+		}
+		else
+		{
+			throw new InvalidPfmFileFormat(message);
+		}
+	}
+	
+	private static void WriteFloat(Stream outputStream, float value, double endianness = -1.0)
+	{
+		var sequence = BitConverter.GetBytes(value);
+		
+		// If the machine is not little-endianness, reverse the sequence of bytes
+		if (endianness == 1.0 && BitConverter.IsLittleEndian)
+		{
+			Array.Reverse(sequence);
+		}
+		outputStream.Write(sequence, 0, sequence.Length);
+	}
+
+	public void WritePfm(Stream outputStream, double endianness = -1.0)
+	{
+		string endiannessString = (endianness == -1.0) ? "-1.0" : "+1.0";
+		
+		// Write the PFM file header (ascii -> does not change with endianness)
+		var header = Encoding.ASCII.GetBytes($"PF\n{Width} {Height}\n{endiannessString}\n");
+		outputStream.Write(header, 0, header.Length); 
+		
+		// Write the image (bottom-to-up, left-to-right)
+		for (int y = Height - 1; y >= 0; --y)
+		{
+			for (int x = 0; x < Width; ++x)
+			{
+				Color color = get_pixel(x, y);
+				WriteFloat(outputStream, color.R, endianness);
+				WriteFloat(outputStream, color.G, endianness);
+				WriteFloat(outputStream, color.B, endianness);
+			}
+		}
+	}
 }
 
 public class InvalidPfmFileFormat : FormatException
