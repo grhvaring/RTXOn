db3a209770b6248aa81c73fa0ca71611d2d5e4da
andrea13 fisica <andreamastro13@gmail.com>
2022-03-30 12:39:47 +0200

    Added struct Vec and Normal with constructors

---
+++ RTXLib/Normal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace RTXLib;
+
+public struct Normal
+{
+	private Vector3 normal;
+
+	// Only for reading the variable
+	public readonly float X => normal.X;
+	public readonly float Y => normal.Y;
+	public readonly float Z => normal.Z;
+
+	// Creating a new normal with all componenents set to 0
+	public Normal()
+	{
+		normal = new Vector3(0);
+	}
+
+	// Creating a new normal with all componenents set to assigned values
+	public Normal(float x, float y, float z)
+	{
+		normal = new Vector3(x, y, z);
+	}
+
+	// Creating a new normal equal to a C# vector
+	public Normal(Vector3 vector)
+	{
+		normal = new Vector3(vector.X, vector.Y, vector.Z);
+	}
+
+	// Creating a new normal equal to another normal
+	public Normal(Normal vector)
+	{
+		normal = new Vector3(vector.X, vector.Y, vector.Z);
+	}
+}
---
+++ RTXLib/Vec.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace RTXLib;
+
+public struct Vec
+{
+	private Vector3 vec;
+
+	// Only for reading the variable
+	public readonly float X => vec.X;
+	public readonly float Y => vec.Y;
+	public readonly float Z => vec.Z;
+
+	// Creating a new 3d vector with all componenents set to 0
+	public Vec()
+	{
+		vec = new Vector3(0);
+	}
+
+	// Creating a new 3d vector with all componenents set to assigned values
+	public Vec(float x, float y, float z)
+    {
+		vec = new Vector3(x, y, z);
+    }
+
+	// Creating a new 3d vector equal to a C# vector
+	public Vec(Vector3 vector)
+	{
+		vec = new Vector3(vector.X, vector.Y, vector.Z);
+	}
+
+	// Creating a new 3d vector equal to another 3D vector
+	public Vec(Vec vector)
+	{
+		vec = new Vector3(vector.X, vector.Y, vector.Z);
+	}
+}
