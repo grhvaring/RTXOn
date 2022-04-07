d7dcf2558f06588fa483dfe33dd320f24f12098a
grhvaring <grhvaring@gmail.com>
2022-04-07 16:12:53 +0200

    Add Struct Point, which describes a "vertex" in 3D space

--- RTXLib/Point.cs
+++ RTXLib/Point.cs
@@ -37,4 +37,67 @@ public struct Point
     {
         point = new Vector3(p.X, p.Y, p.Z);
     }
+    
+    public override string ToString()
+    {
+        return $"Point(x={X}, y={Y}, z={Z})";
+    }
+    
+    public bool IsClose(Point otherPoint, double epsilon = 1e-5)
+    {
+        return (Math.Abs(X - otherPoint.X) < epsilon) && (Math.Abs(Y - otherPoint.Y) < epsilon) && (Math.Abs(Z - otherPoint.Z) < epsilon);
+    }
+    
+    public static Point operator +(Point p, Vec v)
+    {
+        return new Point(p.X + v.X, p.Y + v.Y, p.Z + v.Z);
+    }
+    
+    public static Point operator +(Vec v, Point p)
+    {
+        return p + v;
+    }
+
+    public static Point operator *(float a, Point p)
+    {
+        return new Point(a * p.X, a * p.Y, a * p.Z);
+    }
+    
+    public static Point operator *(Point p, float a)
+    {
+        return a * p;
+    }
+    
+    public static Point operator /(Point p, float a)
+    {
+        return p * (1/a);
+    }
+    
+    public static Point operator -(Point p)
+    {
+        return (-1) * p;
+    }
+    
+    public static Vec operator -(Point p1, Point p2)
+    {
+        // can not use p1 + (-p2) because the difference of two points is a vector
+        // while p1 + (-p2) returns a point
+        return new Vec(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+    }
+    
+    public static Point operator -(Point p, Vec v)
+    {
+        return new Point(p.X - v.X, p.Y - v.Y, p.Z - v.Z);
+    }
+
+    public static Point operator -(Vec v, Point p)
+    {
+        // assume uninary operator '-' is defined for Vec
+        return -(p - v); // v - p = -(p - v)
+    }
+    
+    public Vec ToVec()
+    {
+        return new Vec(X, Y, Z);
+    }
 }
