704b3d3f20ffcd67649d231331f2cef95f048801
andrea13 fisica <andreamastro13@gmail.com>
2022-04-13 12:24:47 +0200

    Added Ray struct

---
+++ RTXLib/Ray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using System.Numerics;
+
+namespace RTXLib;
+
+public struct Ray
+{
+	// Fields
+	public Point Origin;
+	public Vec Dir;
+	public float TMin;
+	public float TMax;
+	public int Depth;
+
+
+	// Default constructor
+	public Ray()
+    {
+		Origin = new Point();
+		Dir = new Vec();
+		TMin = 1e-5f;
+		TMax = Single.PositiveInfinity;
+		Depth = 0;
+	}
+
+	// Constructor with arguments
+	public Ray(Point originPoint, Vec directionVector, float minimumDistance = 1e-5f, float maximumDistance = Single.PositiveInfinity, int numReflection = 0)
+    {
+		Origin = originPoint;
+		Dir = directionVector;
+		TMin = minimumDistance;
+		TMax = maximumDistance;
+		Depth = numReflection;
+	}
+
+	// IsClose checks if two rays have origin and dir close
+	public bool IsClose(Ray otherRay, double epsilon = 1e-5)
+    {
+		return (Origin.IsClose(otherRay.Origin, epsilon) && Dir.IsClose(otherRay.Dir, epsilon));
+    }
+
+	// At calculates the point reached from a ray for a given t
+	public Point At(float t)
+    {
+		return new Point(Origin + t*Dir);
+    }
+}
