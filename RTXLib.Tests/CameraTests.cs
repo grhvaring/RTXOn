a19eb46bafaa186e044ed977a28e5c55ce7106a7
grhvaring <grhvaring@gmail.com>
2022-04-13 15:55:26 +0200

    Add <c>Camera</c>, <code>OrthogonalCamera</code> and PerspectiveCamera types, with tests

---
+++ RTXLib.Tests/CameraTests.cs
@@ -0,0 +1,80 @@
+using Xunit.Abstractions;
+
+namespace RTXLib.Tests;
+using Xunit;
+
+public class CameraTests
+{
+    
+    // Visualization of outputs during a test
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public CameraTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    [Fact]
+    public void TestOrthogonalCamera()
+    {
+        var aspectRatio = 2;
+        var camera = new OrthogonalCamera(aspectRatio, new Transformation());
+        var ray1 = camera.FireRay(0, 0);
+        var ray2 = camera.FireRay(1, 0);
+        var ray3 = camera.FireRay(0, 1);
+        var ray4 = camera.FireRay(1, 1);
+
+        // Check if the rays are parallel 
+        Assert.True(MyLibrary.IsZero(Vec.CrossProduct(ray1.Dir, ray2.Dir).SquaredNorm()));
+        Assert.True(MyLibrary.IsZero(Vec.CrossProduct(ray1.Dir, ray3.Dir).SquaredNorm()));
+        Assert.True(MyLibrary.IsZero(Vec.CrossProduct(ray1.Dir, ray4.Dir).SquaredNorm()));
+        
+        // Check if the rays hit the corners in the correct coordinates
+        Assert.True(ray1.At(1).IsClose(new Point(0, aspectRatio, -1)));
+        Assert.True(ray2.At(1).IsClose(new Point(0, -aspectRatio, -1)));
+        Assert.True(ray3.At(1).IsClose(new Point(0, aspectRatio, 1)));
+        Assert.True(ray4.At(1).IsClose(new Point(0, -aspectRatio, 1)));
+    }
+
+    [Fact]
+    public void TestOrthogonalCameraTransform()
+    {
+        var transformation = Transformation.Translation(-2 * Vec.Ey) * Transformation.RotationZ(90);
+        var camera = new OrthogonalCamera(1, transformation);
+        var ray = camera.FireRay(0.5f, 0.5f);
+        //_testOutputHelper.WriteLine(ray.At(1).ToString());
+        Assert.True(ray.At(1).IsClose(new Point(0, -2, 0)));
+    }
+
+    [Fact]
+    public void TestPerspectiveCamera()
+    {
+        var aspectRatio = 2;
+        var camera = new PerspectiveCamera(1, aspectRatio);
+
+        var ray1 = camera.FireRay(0, 0);
+        var ray2 = camera.FireRay(1, 0);
+        var ray3 = camera.FireRay(0, 1);
+        var ray4 = camera.FireRay(1, 1);
+
+        // Check that the rays have the same origin
+        Assert.True(ray1.Origin.IsClose(ray2.Origin));
+        Assert.True(ray1.Origin.IsClose(ray3.Origin));
+        Assert.True(ray1.Origin.IsClose(ray4.Origin));
+        
+        // Check that the rays hit the corners in the correct coordinates
+        Assert.True(ray1.At(1).IsClose(new Point(0, aspectRatio, -1)));
+        Assert.True(ray2.At(1).IsClose(new Point(0, -aspectRatio, -1)));
+        Assert.True(ray3.At(1).IsClose(new Point(0, aspectRatio, 1)));
+        Assert.True(ray4.At(1).IsClose(new Point(0, -aspectRatio, 1)));
+    }
+    
+    [Fact]
+    public void TestPerspectiveCameraTransform()
+    {
+        var transformation = Transformation.Translation(-2 * Vec.Ey) * Transformation.RotationZ(90);
+        var camera = new PerspectiveCamera(1, 2, transformation);
+        var ray = camera.FireRay(0.5f, 0.5f);
+        //_testOutputHelper.WriteLine(ray.At(1).ToString());
+        Assert.True(ray.At(1).IsClose(new Point(0, -2, 0)));
+    }
+}
