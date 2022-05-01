0e8effce6b0f071f82455436087546a6bcfe88a9
grhvaring <grhvaring@gmail.com>
2022-05-01 11:38:48 +0200

    Add Vec2D struct, used for surface parametrization

---
+++ RTXLib/Vec2D.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+namespace RTXLib;
+
+public struct Vec2D
+{
+
+	public float U { get; set; }
+	public float V { get; set; }
+
+	public static Vec2D Ex => new (1, 0);
+	public static Vec2D Ey => new (0, 1);
+
+	// *** Constructors *** //
+	
+
+	public Vec2D(float u = 0, float v = 0)
+    {
+		U = u;
+		V = v;
+    }
+	
+	public Vec2D(Vec2D vector)
+	{
+		U = vector.U;
+		V = vector.V;
+	}
+	
+	public bool IsClose(Vec2D otherVector, double epsilon = 1e-5)
+	{
+		return Math.Abs(U - otherVector.U) < epsilon && Math.Abs(V - otherVector.V) < epsilon;
+	}
+
+	public override string ToString()
+	{
+		return $"(u= {U}, v= {V})";
+	}
+}
