04e23f1eb8904e24e6ec41558ba61bd260baf62a
andrea13 fisica <andreamastro13@gmail.com>
2022-05-21 15:58:49 +0200

    Correction of a wrong normalization in the generation of random float number of the PCG (all number generated were equal to zero) and upload of test for the ONB

--- RTXLib.Tests/ONBTests.cs
+++ RTXLib.Tests/ONBTests.cs
@@ -21,12 +21,9 @@ namespace RTXLib.Tests
             for (int i = 0; i < 100; i++)
             {
                 Vec normal = new Vec(pcg.RandomFloat(), pcg.RandomFloat(), pcg.RandomFloat());
-                normal.Normalize();
+                normal = normal.Normalize();
+
                 var (e1, e2, e3) = MyLib.CreateONBFromZ(normal);
-                Output.WriteLine(e1.ToString());
-                Output.WriteLine(e2.ToString());
-                Output.WriteLine(e3.ToString());
-                Output.WriteLine(normal.ToString());
 
                 // Verify that the z axis is aligned with the normal
                 Assert.True(e3.IsClose(normal));
@@ -39,14 +36,11 @@ namespace RTXLib.Tests
                 // Verify that each component is normalized
                 Assert.True(MyLib.AreClose(e1.SquaredNorm(), 1.0f));
                 Assert.True(MyLib.AreClose(e2.SquaredNorm(), 1.0f));
-                //Output.WriteLine(i.ToString());
-                //Output.WriteLine(e3.ToString());
                 Assert.True(MyLib.AreClose(e3.SquaredNorm(), 1.0f));
 
                 // Verify that the base has right handness
-                //var v = Vec.CrossProduct(e1, e2);
-
-                //Assert.True(v * e3 > 0);
+                var v = Vec.CrossProduct(e1, e2);
+                Assert.True(v * e3 > 0);
             }
         }
     }
