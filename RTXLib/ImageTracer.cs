5189b44ead7ad930b15219662be3fe36f15fdfc1
Giacomo Riccardo Hvaring <37386002+grhvaring@users.noreply.github.com>
2022-06-06 18:11:24 +0200

    Add antialiasing (#16)

--- RTXLib/ImageTracer.cs
+++ RTXLib/ImageTracer.cs
@@ -18,17 +18,35 @@ public class ImageTracer
         return Camera.FireRay(u, v);
     }
 
-    public void FireAllRays(Func<Ray, Color> function)
+    public void FireAllRays(Func<Ray, Color> function, int numSubDivisions = 0, PCG? pcg = null)
     {
         using var progress = new ProgressBar();
+        var numSubRays = (int) Math.Pow(numSubDivisions + 1, 2);
+        pcg ??= new PCG();
         for (int row = 0; row < Image.Height; ++row)
         {
             SaveSnapShotImage();
             progress.Report((double) row / Image.Height);
             for (int col = 0; col < Image.Width; ++col)
             {
-                var ray = FireRay(col, row);
-                var color = function(ray);
+                var color = new Color();
+
+                if (numSubDivisions == 0)
+                {
+                    var ray = FireRay(col, row);
+                    color = function(ray);
+                }
+                else
+                {
+                    for (var i = 0; i < numSubRays; ++i)
+                    {
+                        // (uPixel, vPixel) specifies a uniformly distributed random point inside the sub square
+                        var (u, v) = (pcg.RandomFloat(), pcg.RandomFloat());
+                        var ray = FireRay(col, row, u, v);
+                        color += function(ray);
+                    }
+                    color /= numSubRays;
+                }
                 Image.SetPixel(col, row, color);
             }
         }
@@ -36,10 +54,11 @@ public class ImageTracer
 
     private void SaveSnapShotImage()
     {
-        var tempImage = Image.ShallowCopy();
-        tempImage.WritePfm("temp.pfm");
+        // to improve
+        Image.WritePfm("temp.pfm");
+        /*var tempImage = Image.ShallowCopy();
         tempImage.NormalizeImage(1);
         tempImage.ClampImage();
-        tempImage.SaveAsPng("temp.png", 1);
+        tempImage.SaveAsPng("temp.png", 1);*/
     }
 }
